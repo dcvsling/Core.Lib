81e5ff63b84b148f31351523278756c465f718ae
彥凱 程 <dcvsling@dcvs.tech>
2019-04-19 01:52:49 +0800

    save

--- Core.Lib.AppParts/ApplicationPartExtensions.cs
+++ Core.Lib.AppParts/ApplicationPartExtensions.cs
@@ -1,12 +1,14 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
-using System.Reflection;
 using Core.Lib.Reflections;
+using Microsoft.AspNetCore.Rest;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyModel;
 using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace Microsoft.AspNetCore.Mvc.ApplicationParts
 {
@@ -20,8 +22,21 @@ namespace Microsoft.AspNetCore.Mvc.ApplicationParts
                     .Configure<IEnumerable<ApplicationPart>>(
                         (m, ps) => ps.Each(m.ApplicationParts.Add))
                 .Services
-                .AddSingleton(p => p.GetRequiredService<IOptions<ApplicationPartManager>>().Value);
+                .AddSingleton(p => p.GetRequiredService<IOptions<ApplicationPartManager>>().Value)
 
+                .AddDefaultAssemblyParts();
 
+        private static IServiceCollection AddParts(this IServiceCollection services, string name = Empty.String, params AssemblyName[] assemblies)
+            => services.Configure<ApplicationFeature>(
+                name,
+                feature => feature.AssemblyNames.AddRange(assemblies));
+
+        private static IServiceCollection AddDefaultAssemblyParts(this IServiceCollection services)
+            => services.AddParts(
+                name: Empty.String,
+                assemblies: DependencyContext
+                .Default
+                .GetDefaultAssemblyNames()
+                .ToArray());
     }
 }
